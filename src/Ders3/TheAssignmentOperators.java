4b5099bba20db21256088402fff1b03004d0e5a0
AysuAlipashayeva <122144338+AysuAlipashayeva@users.noreply.github.com>
2024-04-28 22:07:10 +0400

    assignment

---
+++ src/Ders3/TheAssignmentOperators.java
@@ -0,0 +1,22 @@
+package Ders3;
+
+public class TheAssignmentOperators {
+    public static void main(String[] args) {
+        int i = 23;
+        System.out.println(i); //23
+        i = i + 4;
+        System.out.println(i); //27
+        i = i - 5;
+        System.out.println(i); //22
+        i = i * 3;
+        System.out.println(i); //66
+        i = i % 4;
+        System.out.println(i); //2
+        System.out.println(3 % 2); //1
+        i += 12;
+        System.out.println(i); //14
+        i %= 3;
+        System.out.println(i); //2
+    }
+
+}

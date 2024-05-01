b67236f946a97169bcb88c159f57983d0116eb67
AysuAlipashayeva <122144338+AysuAlipashayeva@users.noreply.github.com>
2024-05-02 00:16:15 +0400

    Dərs praktikası-2(5)

---
+++ src/Ders4/Task5.java
@@ -0,0 +1,12 @@
+package Ders4;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int i=789;
+        int a=i/100;
+        int b=i/10%10;
+        int c=i%10;
+        System.out.println("netice: "+(a+b+c));
+    }
+
+}

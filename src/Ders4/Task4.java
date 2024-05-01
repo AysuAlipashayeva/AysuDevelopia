104739821de78f065dc7cb89ff0c549412f47d2c
AysuAlipashayeva <122144338+AysuAlipashayeva@users.noreply.github.com>
2024-05-01 23:57:59 +0400

    Dərs praktikası-2(4)

---
+++ src/Ders4/Task4.java
@@ -0,0 +1,13 @@
+package Ders4;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Radiusu daxil edin:");
+        double r = sc.nextDouble();
+        double l = 2 * 3.14 * r;
+        System.out.println("l= " + l);
+    }
+}

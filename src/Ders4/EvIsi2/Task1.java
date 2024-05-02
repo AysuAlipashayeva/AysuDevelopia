b5933fa95ba24fc62b11934306532e5587ae7be6
AysuAlipashayeva <122144338+AysuAlipashayeva@users.noreply.github.com>
2024-05-02 11:41:29 +0400

    evIsi-2(1)

---
+++ src/Ders4/EvIsi2/Task1.java
@@ -0,0 +1,9 @@
+package Ders4.EvIsi2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        for (int i = 16; i < 40; i++) {
+            System.out.println(i);
+        }
+    }
+}

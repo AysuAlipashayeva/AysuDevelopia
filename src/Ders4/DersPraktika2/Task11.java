71eac713b0a1f338aade74695456edf6c789173b
AysuAlipashayeva <122144338+AysuAlipashayeva@users.noreply.github.com>
2024-05-02 12:23:20 +0400

    Ders Praktika - 2(11)

--- src/Ders4/DersPraktika2/Task11.java
+++ src/Ders4/DersPraktika2/Task11.java
@@ -1,9 +1,23 @@
 package Ders4.DersPraktika2;
 
-import java.util.Scanner;
 
 public class Task11 {
     public static void main(String[] args) {
-
+        int count = 0;
+        int eded = 2;
+        while (count < 20) {
+            boolean sadeEded = true;
+            for (int i = 2; i <= Math.sqrt(eded); i++) {
+                if (eded % i == 0) {
+                    sadeEded = false;
+                    break;
+                }
+            }
+            if (sadeEded){
+                System.out.println(eded+" ");
+                count++;
+            }
+            eded++;
+        }
     }
 }

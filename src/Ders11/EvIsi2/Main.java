61f41a4bea1fc2432c8f6a715bd00c789db4181c
AysuAlipashayeva <122144338+AysuAlipashayeva@users.noreply.github.com>
2024-06-01 13:53:49 +0400

    ders12-ev isi 2

---
+++ src/Ders11/EvIsi2/Main.java
@@ -0,0 +1,19 @@
+package Ders11.EvIsi2;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee person1 = new Employee();
+        person1.id = 1;
+        person1.name = "Aysu";
+        person1.surname = "Alipashayeva";
+        person1.age = 18;
+        person1.phone = "777601500";
+        person1.address = "Baki";
+        person1.salary = 5000;
+        person1.username = "aalipashyeva";
+        person1.password = 1111111;
+        person1.department = "Engineering";
+        person1.printInfo();
+
+    }
+}

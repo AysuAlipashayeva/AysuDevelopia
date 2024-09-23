f4f7c7b618ad57172216f2b21a67a814a96e7338
AysuAlipashayeva <122144338+AysuAlipashayeva@users.noreply.github.com>
2024-09-23 14:13:35 +0400

    spring project created

---
+++ librarian-aysu/librarian-aysu/src/main/java/az/developia/librarian_aysu/LibrarianAysuApplication.java
@@ -0,0 +1,13 @@
+package az.developia.librarian_aysu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarianAysuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarianAysuApplication.class, args);
+	}
+
+}
---
+++ librarian-aysu/librarian-aysu/src/test/java/az/developia/librarian_aysu/LibrarianAysuApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.librarian_aysu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarianAysuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

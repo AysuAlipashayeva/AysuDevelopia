4d4dbc289a5fef18a53df203c7529379d275834d
AysuAlipashayeva <122144338+AysuAlipashayeva@users.noreply.github.com>
2024-10-03 17:16:01 +0400

    qeydiyyati duzeltdik postmappingle

--- librarianaysu/src/main/java/az/developia/librarianaysu/controller/BookRestController.java
+++ librarianaysu/src/main/java/az/developia/librarianaysu/controller/BookRestController.java
@@ -1,14 +1,23 @@
 package az.developia.librarianaysu.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import az.developia.librarianaysu.request.BookAddRequestDTO;
 import az.developia.librarianaysu.service.BookService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/books")
-
+@RequiredArgsConstructor
 public class BookRestController {
-	private BookService service;
+	private final BookService service;
+	
+	@PostMapping
+	public void add(@RequestBody BookAddRequestDTO req) {
+		service.add(req);
+	}
 
 }

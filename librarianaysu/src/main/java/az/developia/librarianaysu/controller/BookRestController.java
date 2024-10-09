aad896c1bbad20d8a1f0d42d017d8fd01e5424aa
AysuAlipashayeva <122144338+AysuAlipashayeva@users.noreply.github.com>
2024-10-09 23:42:40 +0400

    response status elave olundu

--- librarianaysu/src/main/java/az/developia/librarianaysu/controller/BookRestController.java
+++ librarianaysu/src/main/java/az/developia/librarianaysu/controller/BookRestController.java
@@ -1,5 +1,6 @@
 package az.developia.librarianaysu.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import az.developia.librarianaysu.exception.OurException;
@@ -25,54 +27,54 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/books")
 @RequiredArgsConstructor
 public class BookRestController {
-	
+
 	private final BookService service;
-	
+
 	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
 	public void add(@Valid @RequestBody BookAddRequestDTO req, BindingResult br) {
 		if (br.hasErrors()) {
-			throw new OurException("the information isn't complete","",br);
+			throw new OurException("the information isn't complete", "", br);
 		}
-		
+
 		service.add(req);
 	}
-	
+
 	@PutMapping
+	@ResponseStatus(code = HttpStatus.OK)
 	public void update(@Valid @RequestBody BookUpdateRequestDTO req, BindingResult br) {
 		if (br.hasErrors()) {
-			throw new OurException("the information isn't complete","",br);
+			throw new OurException("the information isn't complete", "", br);
 		}
-		
+
 		service.update(req);
 	}
 
-	@DeleteMapping(path="/{id}")
+	@DeleteMapping(path = "/{id}")
+	@ResponseStatus(code = HttpStatus.OK)
 	public void delete(@PathVariable Long id) {
-		
-		
+
 		service.deleteById(id);
 	}
-	
-	@GetMapping(path="/{id}")
+
+	@GetMapping(path = "/{id}")
 	public BookResponseDTO findById(@PathVariable Long id) {
-		
-		
+
 		return service.findById(id);
 	}
-	
+
 	@GetMapping
 	public BookListResponseDTO findAll() {
-		
-		
+
 		return service.findAll();
 	}
-	
+
 	@PatchMapping
 	public void updateName(@Valid @RequestBody BookUpdateNameRequestDTO req, BindingResult br) {
 		if (br.hasErrors()) {
-			throw new OurException("the information isn't complete","",br);
+			throw new OurException("the information isn't complete", "", br);
 		}
-		
+
 		service.updateName(req);
 	}
 

acab73fc74124a6537860c9edeed274ded78f76a
AysuAlipashayeva <122144338+AysuAlipashayeva@users.noreply.github.com>
2024-10-10 15:06:18 +0400

    kitablar sqlini elave etdim

---
+++ librarianaysu/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into books
+(name, page_count, author, publish_date) values 
+('Java 23','300','James','2024-09-17'),
+('CSS','200','Wium Lie','1996-12-17'),
+('Spring boot 3','400','Rod Johnson','2022-11-24'),
+('HTML','350','Tim Berners-Lee','1999-12-17');
